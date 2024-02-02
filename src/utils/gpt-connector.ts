c9f28248a149d4cbc6922e9b87603f749dd28d82
qraxiss <qraxiss@gmail.com>
2024-02-02 20:27:11 +0300

    gpt-connector connected

---
+++ src/utils/gpt-connector.ts
@@ -0,0 +1,44 @@
+import { config } from "dotenv";
+import axios from "axios";
+
+config();
+
+export const client = axios.create({
+  baseURL: process.env.GPT_CONNECTOR + "3213",
+});
+
+export async function sendMessageToAssistant(
+  systemKey: string,
+  content: string
+) {
+  var res, data;
+  try {
+    res = await client.post(
+      "/openai/continueCompletion",
+      {
+        message: {
+          content,
+          role: "user",
+        },
+        openaiConfig: {
+          max_tokens: 4096,
+        },
+      },
+      {
+        params: {
+          systemKey,
+        },
+      }
+    );
+
+    data = res.data.result.message.content;
+  } catch (error) {
+    try {
+      data = error.response.data.error.message;
+    } catch (error) {
+      data = error.message;
+    }
+  }
+
+  return data;
+}
